01e953330b0b3f90f4287afcc8dfb9366a6631c9
NeemaJanet <neemajanet100@gmail.com>
2023-03-29 11:15:32 +0300

    first

---
+++ calc.js
@@ -0,0 +1,10 @@
+let baseSalary=document.getElementById(base-salary)
+let nssfOne= document.getElementById(nssf-1)
+let nssftwo= document.getElementById(nssf-2)
+let finalPaye= document.getElementById(final-paye)
+let nhif= document.getElementById(nhif)
+let netPay= document.getElementById(net-pay)
+
+function calculate(){
+    baseSalary.textContent=
+}
